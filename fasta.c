8b8da7ff4e1ffc329a89989b6a4e3b18d0750d66
Rayan Chikhi <chikhi@irisa.fr>
2012-05-01 20:32:13 +0200

    fastq support

--- fasta.c
+++ fasta.c
@@ -7,41 +7,99 @@
 #include "fasta.h"
 #include "kmers.h"
 
-// very basic!
+// parse a set of fasta or fastq reads
 unsigned long get_next_read(FILE *file, char *read)
 {
 	unsigned char line[MAX_TIGHT_SIZE];
 	char *rv;
 	char *p;
+    char *read_buffer = read;
 	int nextchar=0;
-	rv=fgets((char *)line,MAX_TIGHT_SIZE,file); // read comment ('>read00xxxx...\n')
+	rv=fgets((char *)line,MAX_TIGHT_SIZE,file); // read comment ('>read00xxxx...\n') and discard it
 	if (rv==NULL)
 	{
 		printf("(reading read header) error or end of file reached!\n");
-		return 0;
+        exit(1);
 	}
-	rv=fgets((char *)read,MAX_TIGHT_SIZE,file); //
-	if (rv==NULL)
-	{
-		printf("(reading read content) error or end of file reached!\n");
-		return 0;
-	}
-	p = (char *)strchr((char*)read, '\n');
-    if (p) *p = '\0';
-	nextchar=fgetc(file); // cheat, reads the next '>' character in order to induce EOF
-	if (nextchar!='>' && !feof(file))
-	{
-		printf("error: bad input (line='%s', read='%s').\n btw this program cannot process genomes. the input file must be reads, of length <%d, one line per read\n",line,read,MAX_TIGHT_SIZE);
-		exit(1);
-	}
-	if (feof(file)) // end of file, no newline at end of read, return compute read length
-		return strlen(read);
-	if ((unsigned long)(p-read)>MAX_TIGHT_SIZE)
-	{
-		printf("error loading read %s (%p, end of read %p) deduced read length %ld. The read is either too long (> %d), or missing a newline at the end (\\\n)?\n",read,read,p,(unsigned long)(p-read),MAX_TIGHT_SIZE);
-		exit(1);
-	}
-	return (unsigned long)(p-read); // readlen
+    int reading_quality = 0;
+    int readlen = 0;
+    *read='\0';
+    // read the read content (can be multi-line)
+	do
+    {
+        if (reading_quality)
+            rv=fgets((char *)line,MAX_TIGHT_SIZE,file); // read quality and discard it
+        else
+            rv=fgets((char *)read_buffer,MAX_TIGHT_SIZE,file); // read the read content to read_buffer (prevents a strcat)
+
+        if (rv==NULL)
+        {
+            printf("(reading read content) error, or unexpected end of file reached (current read: %s)!\n",read);
+            exit(1);
+        }
+
+        // remove trailing \n char; also update readlen
+        p = (char *)strchr((char*)read_buffer, '\n');
+        if (p) 
+        {
+            *p = '\0';
+            readlen += (int)(p-read_buffer);
+        }
+        else
+            readlen += strlen(read_buffer);
+
+        // remove trailing \r char
+        p = (char *)strchr((char*)read_buffer, '\r');
+        if (p)
+        {
+            *p = '\0';
+            readlen -= 1;
+        }
+
+        // advance the read buffer:
+        read_buffer=read+readlen;
+
+        // reads the next '>|@|+' character to: (i) know whether the read continues, (ii) whether there is a quality score, and (iii) in order to induce EOF
+        do
+            nextchar=fgetc(file);
+        while (nextchar == '\n' || nextchar == '\r' ); // also, keep reading newlines.
+
+        if (feof(file)) // end of file (no newline at end of read)
+            break;
+
+        if (nextchar == '+')
+            reading_quality = 1;
+        else
+        {
+            if ((nextchar != '>') && (nextchar != '@'))
+            {
+                // the read content continues at the next line
+                if (!reading_quality)
+                {
+                    // if it's not quality, append that read nucleotide
+                    readlen++;
+                    *read_buffer=nextchar;
+                    read_buffer++;
+                }
+            }
+            else // the next line is a new read
+                break;
+        }
+
+        if (readlen >= MAX_TIGHT_SIZE)
+        {
+            p = (char *)strchr((char*)line, '\n');
+            if (p) *p = '\0';
+
+            printf("error: bad input (line='%s', read='%s').\nthe input file must be reads, of length <%d\n",line,read,MAX_TIGHT_SIZE);
+            exit(1);
+        }
+    }
+    while (1);
+
+    //printf("reading read %s of length %d (== %d)\n",read,readlen,strlen(read));
+
+	return readlen;
 }
 
 
